54cd29138306e511ae2f53cc9d2e84b293561456
Blazej <n30@poczta.onet.pl>
2024-03-18 10:36:46 +0000

    feat: update title again

--- src/store-front/src/components/TopNav.vue
+++ src/store-front/src/components/TopNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <div class="logo">
-      <a href="/">Azure Pet Supplies v27.0.0</a>
+      <a href="/">Azure Pet Supplies v28.0.0</a>
     </div>
     <button class="hamburger" @click="toggleNav">
       <span class="hamburger-icon"></span>
